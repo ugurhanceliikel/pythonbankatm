6683b4dfd67271dd6020e0f2ffc9da7b261712c5
ugurhanceliikel <ugurhanceliikel@gmail.com>
2018-11-30 14:49:00 +0200

    İlk Güncelleme

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+"""Aritmetik Python ifadelerini yazarak ve <return> 'e basarak 
+çalıştırabilirsiniz.Yalnızca bir IPython'a doğru yazılırken, nesnenin dize olarak 
+temsil edilmesini sağlar:"""
+import numpy as np
+data = {i: np.random.randn() for i in range(7)}
+print(data)
